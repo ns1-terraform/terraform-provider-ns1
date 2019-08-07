aefe873335c1111e87c053bc1b4126f57d215ded
mburtless <mburtless@threepnet.com>
2019-08-07 17:53:35 -0400

    Add request logging
    * Add option to enable request body logging via NS1_DEBUG env var

--- ns1/config.go
+++ ns1/config.go
@@ -1,10 +1,16 @@
 package ns1
 
 import (
+	"bytes"
 	"crypto/tls"
+	"encoding/json"
 	"errors"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 
 	ns1 "gopkg.in/ns1/ns1-go.v2/rest"
 )
@@ -17,6 +23,7 @@ type Config struct {
 
 // Client() returns a new NS1 client.
 func (c *Config) Client() (*ns1.Client, error) {
+	var client *ns1.Client
 	httpClient := &http.Client{}
 	decos := []func(*ns1.Client){}
 
@@ -37,10 +44,68 @@ func (c *Config) Client() (*ns1.Client, error) {
 		httpClient.Transport = tr
 	}
 
-	client := ns1.NewClient(httpClient, decos...)
+	// If NS1_DEBUG is set, log the requests and responses
+	if os.Getenv("NS1_DEBUG") != "" {
+		doer := ns1.Decorate(httpClient, Logging())
+		client = ns1.NewClient(doer, decos...)
+	} else {
+		client = ns1.NewClient(httpClient, decos...)
+	}
+
 	client.RateLimitStrategySleep()
 
 	log.Printf("[INFO] NS1 Client configured for Endpoint: %s", client.Endpoint.String())
 
 	return client, nil
+}
+
+func Logging() ns1.Decorator {
+	return func(d ns1.Doer) ns1.Doer {
+		return ns1.DoerFunc(func(r *http.Request) (*http.Response, error) {
+			log.Printf("%s: %s %s", r.UserAgent(), r.Method, r.URL)
+			log.Printf("Headers: %s", r.Header)
+			var err error
+			if r.Body != nil {
+				r.Body, err = logRequest(r.Body)
+				if err != nil {
+					return nil, err
+				}
+			}
+			return d.Do(r)
+		})
+	}
+}
+
+func logRequest(original io.ReadCloser) (io.ReadCloser, error) {
+	// Handle request contentType
+	var bs bytes.Buffer
+	defer original.Close()
+
+	_, err := io.Copy(&bs, original)
+	if err != nil {
+		return nil, err
+	}
+
+	debugInfo := formatJSON(bs.Bytes())
+	log.Printf("[DEBUG] Request Body: %s", debugInfo)
+
+	return ioutil.NopCloser(strings.NewReader(bs.String())), nil
+
+	return original, nil
+}
+
+func formatJSON(raw []byte) string {
+	var rawData interface{}
+	err := json.Unmarshal(raw, &rawData)
+	if err != nil {
+		log.Printf("[DEBUG] Unable to parse JSON: %s", err)
+		return string(raw)
+	}
+	pretty, err := json.MarshalIndent(rawData, "", "  ")
+	if err != nil {
+		log.Printf("[DEBUG] Unable to re-marshal JSON: %s", err)
+		return string(raw)
+	}
+
+	return string(pretty)
 }
