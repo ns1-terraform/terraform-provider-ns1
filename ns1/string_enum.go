593708350a3f4b2c3a3f3ba387ab9b4290cc0149
rupa deadwyler <rdeadwyler@ns1.com>
2019-09-16 10:20:52 -0400

    Address complaints from golint

--- ns1/string_enum.go
+++ ns1/string_enum.go
@@ -5,11 +5,14 @@ import (
 	"strings"
 )
 
+// StringEnum stores a list of strings as keys in a Map, and as a quoted join.
+// Has methods for conveniently testing for the presence of a string.
 type StringEnum struct {
 	ValueMap  map[string]int
 	Expecting string
 }
 
+// NewStringEnum makes a StringEnum from a list of strings.
 func NewStringEnum(values []string) *StringEnum {
 	valueMap := make(map[string]int)
 	quoted := make([]string, len(values), len(values))
@@ -29,15 +32,16 @@ func NewStringEnum(values []string) *StringEnum {
 	}
 }
 
+// Check if string is in our list.
 func (se *StringEnum) Check(v string) (int, error) {
 	i, present := se.ValueMap[v]
 	if present {
 		return i, nil
-	} else {
-		return -1, fmt.Errorf("expecting one of %s; got %q", se.Expecting, v)
 	}
+	return -1, fmt.Errorf("expecting one of %s; got %q", se.Expecting, v)
 }
 
+// ValidateFunc (schema helper)
 func (se *StringEnum) ValidateFunc(v interface{}, k string) (ws []string, es []error) {
 	_, err := se.Check(v.(string))
 	if err != nil {
