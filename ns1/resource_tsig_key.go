e2c7b7a87775b3217336182f9b21d4402102b988
Thiago Uehara <tuehara@daitan.com>
2021-10-01 13:57:14 -0300

    Added ns1_tsigkey read

--- ns1/resource_tsig_key.go
+++ ns1/resource_tsig_key.go
@@ -1,6 +1,9 @@
 package ns1
 
 import (
+	"errors"
+	"log"
+
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 
 	ns1 "gopkg.in/ns1/ns1-go.v2/rest"
@@ -25,7 +28,8 @@ func tsigKeyResource() *schema.Resource {
 
 	return &schema.Resource{
 		Schema:        s,
-		Create:        PulsarJobCreate,
+		Create:        TsigKeyCreate,
+		Read:          tsigKeyRead,
 		SchemaVersion: 1,
 	}
 }
@@ -58,4 +62,24 @@ func TsigKeyCreate(d *schema.ResourceData, meta interface{}) error {
 	}
 
 	return tsigKeyToResourceData(d, &k)
+}
+
+// tsigKeyRead reads the given TSIG key from ns1
+func tsigKeyRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*ns1.Client)
+	k, resp, err := client.TSIG.Get(d.Id())
+	if err != nil {
+		if errors.Is(err, ns1.ErrTsigKeyMissing) {
+			log.Printf("[DEBUG] NS1 TSIG Key (%s) not found", d.Id())
+			d.SetId("")
+			return nil
+		}
+
+		return ConvertToNs1Error(resp, err)
+	}
+	// Set Terraform resource data from the tsig key data we just downloaded
+	if err := tsigKeyToResourceData(d, k); err != nil {
+		return err
+	}
+	return nil
 }
