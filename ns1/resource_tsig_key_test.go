281b2a9327ca897ad53d6ad6e2e3a8a507a53325
Thiago Uehara <tuehara@daitan.com>
2021-10-04 17:17:10 -0300

    Added TSIG key acceptance tests

---
+++ ns1/resource_tsig_key_test.go
@@ -0,0 +1,213 @@
+package ns1
+
+import (
+	"fmt"
+	"log"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+
+	ns1 "gopkg.in/ns1/ns1-go.v2/rest"
+	"gopkg.in/ns1/ns1-go.v2/rest/model/dns"
+)
+
+// Creating TSIG Key
+func TestAccTsigKey_basic(t *testing.T) {
+	var (
+		key          = dns.Tsig_key{}
+		keyName      = fmt.Sprintf("terraform-test-%s.io", acctest.RandStringFromCharSet(15, acctest.CharSetAlphaNum))
+		keyAlgorithm = "hmac-sha256"
+		keySecret    = fmt.Sprintf("terraform-test-%s==", acctest.RandStringFromCharSet(15, acctest.CharSetAlphaNum))
+	)
+	// Basic test for TSIG Key
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckTsigKeyDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccTsigKeyBasic(keyName, keyAlgorithm, keySecret),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckTsigKeyExists("ns1_tsigkey.it", &key),
+					testAccCheckTsigKeyName(&key, keyName),
+					testAccCheckTsigKeyAlgorithm(&key, keyAlgorithm),
+					testAccCheckTsigKeySecret(&key, keySecret),
+				),
+			},
+		},
+	})
+}
+
+// Updating TSIG Keys
+func TestAccTsigKey_updated(t *testing.T) {
+	var (
+		key          = dns.Tsig_key{}
+		keyName      = fmt.Sprintf("terraform-test-%s.io", acctest.RandStringFromCharSet(15, acctest.CharSetAlphaNum))
+		keyAlgorithm = "hmac-sha256"
+		keySecret    = fmt.Sprintf("terraform-test-%s.io==", acctest.RandStringFromCharSet(15, acctest.CharSetAlphaNum))
+
+		updatedAlgorithm = "hmac-sha1"
+		updatedSecret    = fmt.Sprintf("terraform-test-%s.io==", acctest.RandStringFromCharSet(15, acctest.CharSetAlphaNum))
+	)
+
+	// Updating TSIG Key
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckTsigKeyDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccTsigKeyBasic(keyName, keyAlgorithm, keySecret),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckTsigKeyExists("ns1_tsigkey.it", &key),
+					testAccCheckTsigKeyName(&key, keyName),
+					testAccCheckTsigKeyAlgorithm(&key, keyAlgorithm),
+					testAccCheckTsigKeySecret(&key, keySecret),
+				),
+			},
+			{
+				Config: testAccTsigKeyBasic(keyName, updatedAlgorithm, updatedSecret),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckTsigKeyExists("ns1_tsigkey.it", &key),
+					testAccCheckTsigKeyAlgorithm(&key, updatedAlgorithm),
+					testAccCheckTsigKeySecret(&key, updatedSecret),
+				),
+			},
+		},
+	})
+}
+
+// Manually deleting TSIG Key
+func TestAccTsigKey_ManualDelete(t *testing.T) {
+	var (
+		key          = dns.Tsig_key{}
+		keyName      = fmt.Sprintf("terraform-test-%s.io", acctest.RandStringFromCharSet(15, acctest.CharSetAlphaNum))
+		keyAlgorithm = "hmac-sha256"
+		keySecret    = fmt.Sprintf("terraform-test-%s.io==", acctest.RandStringFromCharSet(15, acctest.CharSetAlphaNum))
+	)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckTsigKeyDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccTsigKeyBasic(keyName, keyAlgorithm, keySecret),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckTsigKeyExists("ns1_tsigkey.it", &key),
+				),
+			},
+			// Simulate a manual deletion of the TSIG key and verify that the plan has a diff.
+			{
+				PreConfig:          testAccManualDeleteTsigKey(&key),
+				Config:             testAccTsigKeyBasic(keyName, keyAlgorithm, keySecret),
+				PlanOnly:           true,
+				ExpectNonEmptyPlan: true,
+			},
+			// Then re-create and make sure it is there again.
+			{
+				Config: testAccTsigKeyBasic(keyName, keyAlgorithm, keySecret),
+				Check:  testAccCheckTsigKeyExists("ns1_tsigkey.it", &key),
+			},
+		},
+	})
+}
+
+func testAccTsigKeyBasic(keyName string, keyAlgorithm string, keySecret string) string {
+	return fmt.Sprintf(`resource "ns1_tsigkey" "it" {
+  		name = "%s"
+		algorithm = "%s"
+		secret = "%s"
+}
+`, keyName, keyAlgorithm, keySecret)
+}
+
+func testAccCheckTsigKeyDestroy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*ns1.Client)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "ns1_tsigkey" {
+			continue
+		}
+
+		tsigKey, _, err := client.TSIG.Get(rs.Primary.ID)
+		if err == nil {
+			return fmt.Errorf("TSIG key still exists: %#v: %#v", err, tsigKey)
+		}
+
+	}
+
+	return nil
+}
+
+func testAccCheckTsigKeyExists(n string, key *dns.Tsig_key) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+
+		if !ok {
+			return fmt.Errorf("not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("no ID is set")
+		}
+
+		client := testAccProvider.Meta().(*ns1.Client)
+
+		foundTsigKey, _, err := client.TSIG.Get(rs.Primary.ID)
+
+		p := rs.Primary
+
+		if err != nil {
+			return err
+		}
+
+		if foundTsigKey.Name != p.Attributes["id"] {
+			return fmt.Errorf("TSIG key not found")
+		}
+
+		*key = *foundTsigKey
+
+		return nil
+	}
+}
+
+func testAccCheckTsigKeyName(key *dns.Tsig_key, expected string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		if key.Name != expected {
+			return fmt.Errorf("key.Name: got: %s want: %s", key.Name, expected)
+		}
+		return nil
+	}
+}
+
+func testAccCheckTsigKeyAlgorithm(key *dns.Tsig_key, expected string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		if key.Algorithm != expected {
+			return fmt.Errorf("key.Algorithm: got: %s want: %s", key.Algorithm, expected)
+		}
+		return nil
+	}
+}
+
+func testAccCheckTsigKeySecret(key *dns.Tsig_key, expected string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		if key.Secret != expected {
+			return fmt.Errorf("key.Secret: got: %s want: %s", key.Secret, expected)
+		}
+		return nil
+	}
+}
+
+func testAccManualDeleteTsigKey(key *dns.Tsig_key) func() {
+	return func() {
+		client := testAccProvider.Meta().(*ns1.Client)
+		_, err := client.TSIG.Delete(key.Name)
+		// Not a big deal if this fails, it will get caught in the test conditions and fail the test.
+		if err != nil {
+			log.Printf("failed to delete pulsar job: %v", err)
+		}
+	}
+}
