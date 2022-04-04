0479a19e84d2b6827b46bfc0328cabe6570806db
caiofazio <cayres@daitan.com>
2022-04-04 14:20:36 -0300

    TEG-214 - Removed CaseSensitive tests parallelization to avoid Deadlocks when skipped
    WaitGroups used in parallelization may cause deadlock when skipping the
    test which was causing the timeout

--- ns1/resource_record_test.go
+++ ns1/resource_record_test.go
@@ -7,7 +7,6 @@ import (
 	"regexp"
 	"sort"
 	"strings"
-	"sync"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
@@ -438,6 +437,31 @@ func TestAccRecord_CaseInsensitive(t *testing.T) {
 		zone   string
 	}{
 		{
+			"root level record no cap letters",
+			"terraform-test-#.io",
+			"terraform-test-#.io",
+		},
+		{
+			"root level record domain as title",
+			"Terraform-test-#.io",
+			"terraform-test-#.io",
+		},
+		{
+			"root level record zone as title",
+			"terraform-test-#.io",
+			"Terraform-test-#.io",
+		},
+		{
+			"root level record zone and domain as title",
+			"Terraform-test-#.io",
+			"Terraform-test-#.io",
+		},
+		{
+			"root level record zone and domain random capitalization",
+			"TerrAForm-test-#.io",
+			"TerrafORm-test-#.io",
+		},
+		{
 			"record no cap letters",
 			"test.terraform-test-#.io",
 			"terraform-test-#.io",
@@ -458,9 +482,7 @@ func TestAccRecord_CaseInsensitive(t *testing.T) {
 			"Terraform-test-#.io",
 		},
 	}
-	var wg sync.WaitGroup
 	for _, tt := range CapitalLettersCases {
-		wg.Add(1)
 		var record dns.Record
 
 		rString := acctest.RandStringFromCharSet(15, acctest.CharSetAlphaNum)
@@ -470,94 +492,26 @@ func TestAccRecord_CaseInsensitive(t *testing.T) {
 
 		tfFile := testAccRecordBasicCaseSensitive(domainName, zoneName)
 
-		go func() {
-			resource.Test(t, resource.TestCase{
-				PreCheck:     func() { testAccPreCheck(t) },
-				Providers:    testAccProviders,
-				CheckDestroy: testAccCheckRecordDestroy,
-				Steps: []resource.TestStep{
-					// Simulate an apply
-					{
-						Config: tfFile,
-						Check:  testAccCheckRecordExists("ns1_record.it", &record),
-					},
-					// Simulate a plan to check if has any diff
-					{
-						Config:             tfFile,
-						PlanOnly:           true,
-						ExpectNonEmptyPlan: false,
-					},
+		resource.Test(t, resource.TestCase{
+			PreCheck:     func() { testAccPreCheck(t) },
+			Providers:    testAccProviders,
+			CheckDestroy: testAccCheckRecordDestroy,
+			Steps: []resource.TestStep{
+				// Simulate an apply
+				{
+					Config: tfFile,
+					Check:  testAccCheckRecordExists("ns1_record.it", &record),
 				},
-			})
-			wg.Done()
-		}()
-	}
-	wg.Wait()
-
-}
-
-func TestAccRecord_CaseInsensitive1(t *testing.T) {
-	var CapitalLettersCases = []struct {
-		name   string
-		domain string
-		zone   string
-	}{
-		{
-			"root level record no cap letters",
-			"terraform-test-#.io",
-			"terraform-test-#.io",
-		},
-		{
-			"root level record domain as title",
-			"Terraform-test-#.io",
-			"terraform-test-#.io",
-		},
-		{
-			"root level record zone as title",
-			"terraform-test-#.io",
-			"Terraform-test-#.io",
-		},
-		{
-			"root level record zone and domain as title",
-			"Terraform-test-#.io",
-			"Terraform-test-#.io",
-		},
-	}
-	var wg sync.WaitGroup
-	for _, tt := range CapitalLettersCases {
-		wg.Add(1)
-		var record dns.Record
-
-		rString := acctest.RandStringFromCharSet(15, acctest.CharSetAlphaNum)
-
-		zoneName := strings.Replace(tt.zone, "#", rString, 1)
-		domainName := strings.Replace(tt.domain, "#", rString, 1)
-
-		tfFile := testAccRecordBasicCaseSensitive(domainName, zoneName)
-
-		go func() {
-			resource.Test(t, resource.TestCase{
-				PreCheck:     func() { testAccPreCheck(t) },
-				Providers:    testAccProviders,
-				CheckDestroy: testAccCheckRecordDestroy,
-				Steps: []resource.TestStep{
-					// Simulate an apply
-					{
-						Config: tfFile,
-						Check:  testAccCheckRecordExists("ns1_record.it", &record),
-					},
-					// Simulate a plan to check if has any diff
-					{
-						Config:             tfFile,
-						PlanOnly:           true,
-						ExpectNonEmptyPlan: false,
-					},
+				// Simulate a plan to check if has any diff
+				{
+					Config:             tfFile,
+					PlanOnly:           true,
+					ExpectNonEmptyPlan: false,
 				},
-			})
-			wg.Done()
-		}()
+			},
+		})
+
 	}
-	wg.Wait()
 
 }
 
