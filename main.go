3aa67c0a5e16af45e680deddd873fd56eede7e9e
Ed Ravin <eravin@ns1.com>
2022-12-19 16:15:33 -0500

    make main debuggable

--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"log"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/terraform-providers/terraform-provider-ns1/ns1"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: ns1.Provider})
+	var debugMode bool
+
+	flag.BoolVar(&debugMode, "debuggable", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	if debugMode {
+		err := plugin.Debug(context.Background(), "registry.terraform.io/ns1-terraform/ns1",
+			&plugin.ServeOpts{
+				ProviderFunc: ns1.Provider,
+			})
+		if err != nil {
+			log.Println(err.Error())
+		}
+	} else {
+		plugin.Serve(&plugin.ServeOpts{
+			ProviderFunc: ns1.Provider})
+	}
 }
