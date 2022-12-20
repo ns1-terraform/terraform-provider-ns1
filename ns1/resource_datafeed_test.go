83e011181ea824b321a46d10b35d3fdd8548fb75
Ed Ravin <eravin@ns1.com>
2022-12-20 17:05:09 -0500

    fix fallout from nsone/standalone_core:7c945dba433492f97ba302ec02bf1173a6a69c25

--- ns1/resource_datafeed_test.go
+++ ns1/resource_datafeed_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"reflect"
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
@@ -96,6 +97,7 @@ func TestAccDataFeed_ManualDelete(t *testing.T) {
 				Config:             testAccDataFeedBasic,
 				PlanOnly:           true,
 				ExpectNonEmptyPlan: true,
+				ExpectError:        regexp.MustCompile("GET .*/data/feeds/.* not found"),
 			},
 			// Then re-create and make sure it is there again.
 			{
