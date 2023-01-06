fc6cefc692945a4ec228a4fba5f03902131f2699
Ed Ravin <eravin@ns1.com>
2023-01-06 11:27:30 -0500

    use error codes again, after SDK fixes

--- ns1/resource_apikey.go
+++ ns1/resource_apikey.go
@@ -131,7 +131,7 @@ func ApikeyRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*ns1.Client)
 	k, resp, err := client.APIKeys.Get(d.Id())
 	if err != nil {
-		if resp != nil && resp.StatusCode == 404 {
+		if err == ns1.ErrKeyMissing {
 			log.Printf("[DEBUG] NS1 API key (%s) not found", d.Id())
 			d.SetId("")
 			return nil
