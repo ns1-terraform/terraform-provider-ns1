a31a076dc7ccb87bd2825017612e768b6167621a
Cristian Pontes <cpontes@ns1.com>
2025-04-03 16:47:55 +0100

    Fixing billing-usage resource naming

--- ns1/provider.go
+++ ns1/provider.go
@@ -55,7 +55,7 @@ func Provider() *schema.Provider {
 			"ns1_record":             dataSourceRecord(),
 			"ns1_networks":           dataSourceNetworks(),
 			"ns1_monitoring_regions": dataSourceMonitoringRegions(),
-			"billing_usage":          billingUsageResource(),
+			"ns1_billing_usage":      billingUsageResource(),
 		},
 		ResourcesMap: map[string]*schema.Resource{
 			"ns1_zone":                 resourceZone(),
