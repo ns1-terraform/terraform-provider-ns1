ffcd47e16b75db0ee24c9c58b69dae25acdb492d
Ferdinando Formica <ferdinando.formica@ie.ibm.com>
2025-10-20 14:25:31 +0100

    align white spaces

--- ns1/examples/billing_usage.tf
+++ ns1/examples/billing_usage.tf
@@ -1,11 +1,11 @@
 locals {
-  now           = timestamp()
-  now_unix      = provider["time"].rfc3339_parse(local.now).unix
-  cur_mon = formatdate("YYYY-MM", local.now)
-  begin_mon          = "${local.cur_mon}-01T00:00:00Z"
-  bmon_unix     = provider["time"].rfc3339_parse(local.begin_mon).unix
-  end_mon          = timeadd(local.begin_mon, "720h")
-  emon_unix     = provider["time"].rfc3339_parse(local.end_mon).unix
+  now       = timestamp()
+  now_unix  = provider["time"].rfc3339_parse(local.now).unix
+  cur_mon   = formatdate("YYYY-MM", local.now)
+  begin_mon = "${local.cur_mon}-01T00:00:00Z"
+  bmon_unix = provider["time"].rfc3339_parse(local.begin_mon).unix
+  end_mon   = timeadd(local.begin_mon, "720h")
+  emon_unix = provider["time"].rfc3339_parse(local.end_mon).unix
 }
 
 # Get query usage data for the given timeframe
